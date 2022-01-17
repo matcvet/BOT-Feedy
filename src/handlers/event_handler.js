a9819cfe51f8c31229c40cabdd7142ff28764a14
Stealth <cvetanov.matea@gmail.com>
2022-01-17 19:20:28 +0100

    changed handlers url

--- src/handlers/event_handler.js
+++ src/handlers/event_handler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = (client, Discord, distube) => {
     const loadDir = dirs => {
-        const eventFiles = fs.readdirSync(`./src/events/${dirs}`).filter(file => file.endsWith('.js'));
+        const eventFiles = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith('.js'));
         for(const file of eventFiles) {
             const event = require(`../events/${dirs}/${file}`);
             const eventName = file.split('.')[0];
