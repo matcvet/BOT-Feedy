e2dad0a6b50025eb371c2e36b0cad451c30e53cc
Stealth <cvetanov.matea@gmail.com>
2022-02-01 03:22:47 +0100

    added 2 more buttons to queue list command

--- src/commands/music_commands/queue.js
+++ src/commands/music_commands/queue.js
@@ -12,8 +12,8 @@ const generateQueue = queue => {
                     }\``,
             )
             .slice(i, i + 10)
-            .join('\n')
-        chunks.push(chunk)
+            .join('\n');
+        chunks.push(chunk);
     }
 
     return chunks;
@@ -28,34 +28,48 @@ module.exports = {
 
         if (!msg.member.voice.channel)
             return msg.channel.send('Youre not in the channel 😔');
-        
+
         if (msg.guild.me.voice.channel && msg.member.voice.channel.id !== msg.guild.me.voice.channel.id)
             return msg.channel.send("You must be in the same voice channel to use commands.");
-        
+
         if (!queue)
             return msg.channel.send('No queue available.');
-        
+
 
 
         const row = new MessageActionRow()
             .addComponents(
                 new MessageButton()
+                    .setCustomId('toFirst')
+                    .setLabel('⏪')
+                    .setStyle('PRIMARY')
+            )
+            .addComponents(
+                new MessageButton()
                     .setCustomId('previous')
-                    .setLabel('Previous')
-                    .setStyle('PRIMARY'),
+                    .setLabel('⏮')
+                    .setStyle('PRIMARY')
             )
             .addComponents(
                 new MessageButton()
                     .setCustomId('next')
-                    .setLabel('Next')
-                    .setStyle('PRIMARY'),
+                    .setLabel('⏭')
+                    .setStyle('PRIMARY')
             )
-        
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('toLast')
+                    .setLabel('⏩')
+                    .setStyle('PRIMARY')
+            )
+
         let currentPage = 0;
 
         const queueList = generateQueue(queue);
 
-        const queueMessage = await msg.channel.send({ content: `**${queue.songs.length} songs in queue** \n ${queueList[currentPage]} \n **page: ${currentPage + 1}/${queueList.length}**`, components: [row] });
+        //let showContent = `**${queue.songs.length - 1} songs in queue** \n ${queueList[currentPage]} \n **page: ${currentPage + 1}/${queueList.length}**`
+
+        const queueMessage = await msg.channel.send({ content: `**${queue.songs.length - 1} songs in queue** \n ${queueList[currentPage]} \n **page: ${currentPage + 1}/${queueList.length}**`, components: [row] });
 
         const filter = button => button.customId === 'previous' && button.customId === 'next';
         const collector = queueMessage.createMessageComponentCollector(filter);
@@ -64,12 +78,22 @@ module.exports = {
             if (b.customId === 'next') {
                 if (currentPage < queueList.length - 1) {
                     currentPage += 1;
-                    await b.update({ content: `${queueList[currentPage]} \n **page: ${currentPage + 1}/${queueList.length}**`, components: [row] });
+                    await b.update({ content: `**${queue.songs.length - 1} songs in queue** \n ${queueList[currentPage]} \n **page: ${currentPage + 1}/${queueList.length}**`, components: [row] });
                 }
             } else if (b.customId === 'previous') {
-                if (currentPage !== 0) {
+                if (currentPage != 0) {
                     currentPage -= 1;
-                    await b.update({ content: `${queueList[currentPage]} \n **page: ${currentPage + 1}/${queueList.length}**`, components: [row] });
+                    await b.update({ content: `**${queue.songs.length - 1} songs in queue** \n ${queueList[currentPage]} \n **page: ${currentPage + 1}/${queueList.length}**`, components: [row] });
+                }
+            } else if (b.customId === 'toLast') {
+                if (currentPage < queueList.length - 1) {
+                    currentPage = queueList.length - 1;
+                    await b.update({ content: `**${queue.songs.length - 1} songs in queue** \n ${queueList[currentPage]} \n **page: ${currentPage + 1}/${queueList.length}**`, components: [row] });
+                }
+            } else if (b.customId === 'toFirst') {
+                if (currentPage != 0) {
+                    currentPage = 0;
+                    await b.update({ content: `**${queue.songs.length - 1} songs in queue** \n ${queueList[currentPage]} \n **page: ${currentPage + 1}/${queueList.length}**`, components: [row] });
                 }
             }
         })
