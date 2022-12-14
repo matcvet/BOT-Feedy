73eb40d650de887ad6c99c9c9a6d8a79a899a1a7
Stealth <cvetanov.matea@gmail.com>
2022-12-14 16:11:39 +0100

    Updated ytdl-core

--- src/commands/music_commands/clear.js
+++ src/commands/music_commands/clear.js
@@ -2,22 +2,26 @@ module.exports = {
     name: "clear",
     description: "Clear the queue.",
     async execute(msg, args, Discord, bot) {
-        const queue = bot.getQueue(msg);
+        try {
+            const queue = bot.getQueue(msg);
 
-        if (!msg.member.voice.channel) {
-            return msg.channel.send("You have to join a voice channel first. ❌ ");
+            if (!msg.member.voice.channel) {
+                return msg.channel.send("You have to join a voice channel first. ❌ ");
+            }
+    
+            if (msg.guild.me.voice.channel && msg.member.voice.channel.id !== msg.guild.me.voice.channel.id) {
+                return msg.channel.send("You must be in the same voice channel to use commands. ❌ ");
+            }
+    
+            if (!queue) {
+                return msg.channel.send("Bot is currently not playing. ❌ ");
+            }
+    
+            bot.stop(msg);
+            
+            msg.channel.send("Queue cleared! ✅");
+        } catch(err) {
+            console.error(err);
         }
-
-        if (msg.guild.me.voice.channel && msg.member.voice.channel.id !== msg.guild.me.voice.channel.id) {
-            return msg.channel.send("You must be in the same voice channel to use commands. ❌ ");
-        }
-
-        if (!queue) {
-            return msg.channel.send("Bot is currently not playing. ❌ ");
-        }
-
-        bot.stop(msg);
-        
-        msg.channel.send("Queue cleared! ✅");
     },
 };
